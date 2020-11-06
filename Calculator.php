687bd88e566fe7beed5883aaa9fe341c7e13fa3a
Nam Hong <1714167@dlu.edu.vn>
2020-11-06 08:37:15 +0700

    first commit

---
+++ Calculator.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<style rel="stylesheet" href="style.css"></style>
+	<title></title>
+</head>
+<body>
+	<div class="content"></div>
+		<h1>Mô phỏng máy tính điện tử</h1>
+		<form accept="#"method="post" name="main-form">
+			<div class="row'">
+				<span>Số thứ nhất</span>
+				<input type="text" name="number1"
+			</div>
+				
+			<div class="row'">
+				<span>Phép toán</span>
+				<input type="text" name="calculator"
+			</div>
+
+			<div class="row'">
+				<span>Số thứ hai</span>
+				<input type="text" name="number2"
+			</div>
+
+			<div class="row">
+				<input type="submit" name="Xem kết quả"
+			</div>
+
+			<div class="row">
+				<p>Kết quả:</p>
+			</div>
+
+
+		</form>
+
+</body>
+</html>
